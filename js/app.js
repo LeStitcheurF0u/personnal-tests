3f69e3179c891c6a6ed47f7abd10613911808141
LeStitcheurF0u <mcfly@mywebsoluce.fr>
2023-04-16 07:38:54 +0200

    :iphone: Optimisation responsive

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 const nav = document.querySelector('nav');
 const realisation = document.querySelector('#realisation');
+const hero = document.querySelector('#hero')
 
 window.addEventListener('scroll', () => {
-    let { scrollTop, clientHeight } = document.documentElement
+    let { scrollTop, clientHeight, clientWidth } = document.documentElement
 
     let topElementToTopViewport = realisation.getBoundingClientRect().top
 
+
     if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.90){
-        nav.classList.add('sticky')
+        if(clientWidth > 900){
+            nav.classList.add('sticky')
+        }
     }
     if(scrollTop < (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.90){
-        if(nav.classList.contains('sticky')){
+        if(clientWidth > 900 || nav.classList.contains('sticky')){
             nav.classList.remove('sticky')
         }
     }
