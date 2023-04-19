d334d1a19204d968270eba6a34084dabfdbeb2d1
LeStitcheurF0u <mcfly@mywebsoluce.fr>
2023-04-20 00:32:15 +0200

    :rocket: Deploy pre-release 1.0

---
+++ import/form.php
@@ -0,0 +1,58 @@
+<aside class="contact-form">
+  <form action="traitements/mails.php" method="post" id="form">
+    <p>Une question ? Un projet ? Envoyez moi un message</p>
+
+    <?php
+    if (isset($_SESSION['message']) && !empty($_SESSION['message'])) :
+    ?>
+      <p class='message'><?= $_SESSION['message'] ?></p>
+    <?php
+      unset($_SESSION['message']);
+    endif;
+
+    if (isset($_SESSION['error']) && !empty($_SESSION['error'])) :
+    ?>
+      <p class="error"><?= $_SESSION['error'] ?></p>
+    <?php
+      unset($_SESSION['error']);
+    endif;
+    ?>
+    <div class="contact-form__item">
+      <label for="nom">Nom</label>
+      <input type="text" id="nom" name="nom" required />
+    </div>
+    <div class="contact-form__item">
+      <label for="prenom">Prénom</label>
+      <input type="text" id="prenom" name="prenom" required />
+    </div>
+    <div class="contact-form__item">
+      <label for="mail">Email</label>
+      <input type="email" id="mail" name="mail" required />
+    </div>
+    <div class="contact-form__item">
+      <label for="subject">Sujet de votre message</label>
+      <select name="subject" id="subject" required>
+        <option value="0">Veuillez sélectionner un sujet</option>
+        <option value="1">Demande de devis</option>
+        <option value="2">J'ai un projet web</option>
+        <option value="3">Demande de renseignements</option>
+        <option value="4">Autre</option>
+      </select>
+    </div>
+    <div class="contact-form__item">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" required><?php if (isset($_SESSION['form'])) {
+                                                        echo $_SESSION['form']['message'];
+                                                        unset($_SESSION['form']);
+                                                      }
+                                                      ?></textarea>
+    </div>
+    <div class="contact-form__item">
+      <input type="hidden" name="honeypot" />
+    </div>
+    <div class="contact-form__buttons">
+      <button type="submit">Envoyer</button>
+      <button type="reset">Recommencer</button>
+    </div>
+  </form>
+</aside>
--- traitements/mails.php
+++ traitements/mails.php
@@ -2,17 +2,20 @@
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['honeypot']) && empty($_POST['honeypot'])) {
-            if (!preg_match('!^ *$!s', $_POST['nom'] && !preg_match('!^ *$!s', $_POST['prenom']) && !preg_match('!^ *$!s', $_POST['mail']) && !preg_match('!^ *$!s', $_POST['sujet']) && !preg_match('!^ *$!s', $_POST['sujet']))) {
+            if (!preg_match('!^ *$!s', $_POST['nom'] && !preg_match('!^ *$!s', $_POST['prenom']) && !preg_match('!^ *$!s', $_POST['mail']) && !preg_match('!^ *$!s', $_POST['message']))) {
+                session_start();
                 $mail = strip_tags($_POST['mail']);
                 $nom = strip_tags($_POST['nom']);
                 $prenom = strip_tags($_POST['prenom']);
                 $content = strip_tags($_POST['message']);
 
+                if ($_POST['subject'] == 0 || $_POST['subject'] > 4) {
+                    $_SESSION['error'] = "Vous n'avez pas choisi de sujet valide";
+                    header('location: ../index.php#form');
+                    exit;
+                }
+
                 switch ($_POST['subject']) {
-                    case 0:
-                        $_SESSION['error'] = "Vous n'avez pas choisi de sujet";
-                        header('location: index.php');
-                        break;
                     case 1:
                         $subject = "Demande de devis";
                         break;
@@ -25,11 +28,26 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
                     case 4:
                         $subject = "Autre demande";
                         break;
-                    default:
-                        $_SESSION['error'] = "Ce sujet n'est pas valide";
-                        header('location: index.php');
-                        break;
                 }
+
+                $to = "contact@mywebsoluce.fr";
+                $from = "webmaster@mywebsoluce.fr";
+
+                $message = "<p>Bonjour,</p>
+                            <p>Vous avez reçu un nouveau message de la part de $nom $prenom</p>
+                            <p>Voici le contenu du message :</p>
+                            <p>$content</p>
+                            <p>Vous pouvez répondre à $mail</p>";
+
+                $headers = "from : webmaster@mywebsoluce.fr" . "\r\n" .
+                    'Reply-To: ' . $mail . "\r\n" .
+                    'X-Mailer : PHP/' . phpversion() . "\r\n" .
+                    "Content-Type: text/html; charset=UTF-8";
+
+                mail($to, $subject, $message, $headers);
+
+                $_SESSION['message'] = "Votre message a bien été envoyé. J'y répondrais dans les plus bref délais";
+                header('Location: ../index.php#form');
             }
         }
     }
